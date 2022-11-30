832c672adda502bf1dcf6caeed7fddd25a6885cf
Kurtmj93 <kurtmj93@gmail.com>
2022-11-29 20:49:20 -0500

    create elements, grab elements with jQuery, create questions array

---
+++ script.js
@@ -0,0 +1,40 @@
+// grab HTML elements using JQuery
+let quizEl = $('#quiz');
+let timerEl = $('#timer');
+let timeEl = $('#time');
+let feedbackEl = $('#feedback');
+let messageEl = $('#message');
+
+// array of question objects
+
+let questions = [
+    {
+        "text": "",
+        "mc1": "",
+        "mc2": "",
+        "mc3": "",
+        "mc4": "",
+        "answer": ""
+    } {
+        "text": "",
+        "mc1": "",
+        "mc2": "",
+        "mc3": "",
+        "mc4": "",
+        "answer": ""
+    } {
+        "text": "",
+        "mc1": "",
+        "mc2": "",
+        "mc3": "",
+        "mc4": "",
+        "answer": ""
+    } {
+        "text": "",
+        "mc1": "",
+        "mc2": "",
+        "mc3": "",
+        "mc4": "",
+        "answer": ""
+    }
+];
