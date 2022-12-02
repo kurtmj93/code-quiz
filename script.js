3fe7c905be9c006ccc1d4c9c1c3defd0bdef1091
Kurtmj93 <kurtmj93@gmail.com>
2022-12-01 19:41:06 -0500

    implement working timer

--- script.js
+++ script.js
@@ -4,37 +4,59 @@ let timerEl = $('#timer');
 let timeEl = $('#time');
 let feedbackEl = $('#feedback');
 let messageEl = $('#message');
+let startEl = $('#start')
 
 // array of question objects
 
 let questions = [
     {
-        "text": "",
-        "mc1": "",
-        "mc2": "",
-        "mc3": "",
-        "mc4": "",
-        "answer": ""
-    } {
-        "text": "",
-        "mc1": "",
-        "mc2": "",
-        "mc3": "",
-        "mc4": "",
-        "answer": ""
-    } {
-        "text": "",
-        "mc1": "",
-        "mc2": "",
-        "mc3": "",
-        "mc4": "",
-        "answer": ""
-    } {
-        "text": "",
-        "mc1": "",
-        "mc2": "",
-        "mc3": "",
-        "mc4": "",
-        "answer": ""
+        "text": "Commonly used data types do NOT include: ",
+        "mc1": "Strings",
+        "mc2": "Booleans",
+        "mc3": "Alerts",
+        "mc4": "Numbers",
+        "answer": "Alerts"
+    },{
+        "text": "Arrays in Javascript can be used to store _____________.",
+        "mc1": "Numbers and Strings",
+        "mc2": "Other Arrays",
+        "mc3": "Booleans",
+        "mc4": "All of the Above",
+        "answer": "All of the Above"
+    },{
+        "text": "Click on the answer.",
+        "mc1": "The answer.",
+        "mc2": "Not the answer.",
+        "mc3": "Definitely not the answer.",
+        "mc4": "Don't click here.",
+        "answer": "The answer."
     }
 ];
+
+let score = 0;
+let isFinished = false;
+
+function finishQuiz() {
+    alert("it's over");
+};
+
+function startTimer() {
+    let time = 5;
+    timeEl.text(time);
+    // changes timer css to display with jQuery
+    timerEl.show();
+    // sets timer going
+    let timer = setInterval(function() {
+        time--;
+        timeEl.text(time);
+        // OR operator checks if questions are finished or time has run out
+        if (isFinished || time === 0) {
+            clearInterval(timer);
+            finishQuiz();
+        }
+    }, 1000);
+}
+
+startEl.click(function() {
+    startTimer();
+});
